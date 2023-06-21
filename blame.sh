cd9788ab50a6031761c398d9e426bfa5a4a10266
Gabriel Fournier <gab@gfournier.ca>
2023-06-20 20:52:52 -0400

    doc: Demo Hints

--- blame.sh
+++ blame.sh
@@ -14,3 +14,6 @@ cd $CWD
 git config --global --add safe.directory $CWD
 
 bash
+# git show 8e607c98660bb3b8ae2462b3d7b37a8cd11730aa
+# git-blame-someone-else "Name <Email>" 8e607c98660bb3b8ae2462b3d7b37a8cd11730aa
+# git show 8e607c98660bb3b8ae2462b3d7b37a8cd11730aa
